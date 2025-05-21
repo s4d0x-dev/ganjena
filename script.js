c0be1a0639188943cb9251d03f54f0b66db643e6
S4D0X <faredba@outlook.com>
2025-05-21 16:35:41 +0430

    Updated PWA Feature.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let dictionary = {};
 let deferredPrompt = null;
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(() => console.log('Service Worker Registered'))
+    .catch(err => console.error('Service Worker Registration Failed:', err));
+}
+
 fetch('data/dictionary.json')
   .then(res => res.json())
   .then(data => {
